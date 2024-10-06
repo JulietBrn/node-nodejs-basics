57b0524ab66f1aeaec7d759fb7f15c363e94e1bd
JulietBrn <shishkina.ja@gmail.com>
2024-10-06 15:44:51 +0200

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'fs';
+import { join } from 'path'
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileName = 'fileToCalculateHashFor.txt'
+  const filePath = join('src', 'hash', 'files', fileName);
+  let hash;
+
+  const fileStream = createReadStream(filePath);
+  fileStream.on('data', (chunk) => {
+    hash = createHash('sha256').update(chunk);
+  });
+
+  fileStream.on('end', () => {
+    const fileHash = hash.digest('hex');
+    console.log(`${fileHash}`);
+  });
+
 };
 
 await calculateHash();
