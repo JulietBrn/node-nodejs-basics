c441510e18d6bab19988d95fcdced8365461e62f
JulietBrn <shishkina.ja@gmail.com>
2024-10-06 13:28:58 +0200

    feat: complete cli task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssKeys = Object.keys(process.env).filter(el => {
+    return el.startsWith('RSS');
+  })
+
+  const rssVars = rssKeys.map(key => {
+    return `${key}=${process.env[key]}`
+  })
+
+  console.log(rssVars.join('; '));
 };
 
 parseEnv();
