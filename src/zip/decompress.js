cbb79fa6e3085285752883fe3d576aef495453c0
JulietBrn <shishkina.ja@gmail.com>
2024-10-07 10:59:09 +0200

    feat: complete zip task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { pipeline } from 'stream'
+import { createWriteStream, createReadStream} from 'fs';
+import { join } from 'path';
+import zlib from 'zlib'
+
 const decompress = async () => {
-    // Write your code here 
+
+  const fileNameFrom = 'archive.gz'
+  const filePathFrom = join('src', 'zip', 'files', fileNameFrom);
+
+  const fileNameTo = 'fileToCompress.txt'
+  const filePathTo = join('src', 'zip', 'files', fileNameTo);
+
+  const reedStream = createReadStream(filePathFrom)
+  const writeStream = createWriteStream(filePathTo)
+
+  const compressStream = zlib.createGunzip()
+
+  pipeline(reedStream, compressStream, writeStream, (err) => console.log(err))
 };
 
 await decompress();
