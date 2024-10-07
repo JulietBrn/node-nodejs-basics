27c8772e6febe550d2ec35e31bc2b50572b41409
JulietBrn <shishkina.ja@gmail.com>
2024-10-07 13:32:33 +0200

    feat: complete cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import child_process from 'child_process';
+import { join } from 'path'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathToChild = join('src', 'cp', 'files', 'script.js')
+
+  const child = child_process.fork(pathToChild, args, {
+    stdio: ['pipe', 'pipe', 'inherit', 'ipc'],
+  })
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['1', '2', 10]);
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,3 +1,4 @@
+import { workerData, parentPort } from 'worker_threads'
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
@@ -5,4 +6,5 @@ const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
 };
 
-sendResult();
+// sendResult();
+parentPort.postMessage(sendResult())
